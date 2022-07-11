69d2b9bedf4cc22a2c0fcf10a49c8e05eb035515
EuGlenMalaj <euglen1991@gmail.com>
2022-07-12 00:24:01 +0200

    Ndryshime ne edukim

--- resources/subpages/resume.js
+++ resources/subpages/resume.js
@@ -11,28 +11,59 @@ export default {
         title: 'Specialist in the field of environmental pollution control',
         start: '06/2021',
         end: 'present',
-        company: 'Consumer Protection Agency Municipality of Tirana (Albania)',
-        description: 'Swinging from building to building, fighting bad guys and making a difference in the world.'
+        company: 'Consumer Protection Agency Municipality of Tirana AL',
+        description: 'Experts who monitor the impact of a population on the environment, identifying environmental issues, and recommending solutions.'
       },
       {
         title: 'Electrical Engineer',
         start: '08/2019',
         end: 'present',
-        company: 'Part time Freelance',
+        company: 'Part time Freelance Tirana AL ',
         description: 'Design in AutoCad electrical infrastructure for civil and industrial buildings and Implementation of the designed plan. '
+      },
+      {
+        title: 'Management Team Leader',
+        start: '11/2014',
+        end: '06/2021',
+        company: 'GENA Family Business Ballsh AL',
+        description: 'Leadership of a staff of nine to twelve people        • Create a work plan and delegate work to the team members          • Assess progress and adjust the plan accordingly'
+      },{
+        title: 'Electrical Engineer',
+        start: '04/2018',
+        end: '08/2018',
+        company: 'Industrial automation (Automatizime industriale) Tirana AL',
+        description: '•Work on the construction and testing of electrical / automation panels                •Design in the AutoCAD infrastructure of electrical / automation panels        •Maintenance of industrial printers (trained for printers VIDEOJET USA) '
+      },{
+        title: 'Inspection Specialist',
+        start: '05/2017',
+        end: '07/2017',
+        company: 'National Water and Sewerage Agency',
+        description: 'Inspection and assessment of the investment needs atthe regional director of the Albanian water supply. '
       },
     ]
   },
   education: {
-    title: 'Education',
+    title: 'Educational Path',
     icon: 'mdi-school',
     data: [
       {
-        major: 'Journalism',
-        start: '09/2014',
-        end: '07/2018',
-        institute: 'High School',
-        description: 'Hanging out with Ned and MJ, throwing house parties, city parties and all kind of parties.'
+        major: 'Energy Audit in Buildings',
+        start: '10/2021',
+        end: '04/2022',
+        institute: 'Polytechnic University of Tirana, Tirana AL ',
+        description: 'Postgraduate course'
+      },{
+        major: 'Masters Degree in ElectroPower',
+        start: '07/2014',
+        end: '10/2016',
+        institute: 'Polytechnic University of Tirana, Tirana AL ',
+        description: 'Master thesis: Efficiency of the turbine generator set in the Bishnica hydropower plant'
+      },{
+        major: 'Bachelor in Electrical Engineering',
+        start: '08/2010',
+        end: '03/2014',
+        institute: 'Polytechnic University of Tirana, Tirana AL ',
+        description: 'Bachelor Degree Specializing in Electropower'
       },
     ]
   },
