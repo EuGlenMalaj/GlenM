8443c995d062ed576d11324bc9d5294f6d011c8b
EuGlenMalaj <euglen1991@gmail.com>
2022-07-13 11:10:54 +0200

    ndryshime fillestare te protofoli

--- resources/subpages/portfolio.js
+++ resources/subpages/portfolio.js
@@ -12,15 +12,15 @@ export default {
       description: 'Indoor / Outdoor (Electrical drawing) lighting layout plan.      Power plug layout plan.      AC layout plan    Telephone, television, and computer drawing and layout plan.    Smoke detector and public address system drawing and layout plan.       Lightning protection layout plan.     Generator and transformer to Main Panel layout plan'
     },
     {
-      title: 'Taking pictures',
+      title: '',
       icon: 'mdi-desktop-mac',
-      description: 'Delivering high-end and superb pictures of myself in all different locations.'
+      description: ''
     },
     
     {
-      title: 'Dicka tjeter',
+      title: '',
       icon: 'mdi-diving',
-      description: 'nje pershkrim'
+      description: ''
     }
   ]
 };
