cc7817fdae36ea753e9f9e3a3807d2f016932266
EuGlenMalaj <euglen1991@gmail.com>
2022-07-13 10:51:08 +0200

    Ndryshime ne Services

--- resources/subpages/services.js
+++ resources/subpages/services.js
@@ -1,26 +1,22 @@
 export default {
   name: 'services', // do not change
   pageTitle: 'Services',
-  header: 'Works',
-  icon: 'mdi-briefcase-outline',
+  header: 'Services',
+  icon: 'mdi-monitor-shimmer',
 
   footer: 'Footer , ca te duash',
   products: [
     {
-      title: 'I can do AutoCAD Electrical drawings, schemes and floor plans',
+      title: 'AutoCAD Electrical drawings, schemes and floor plans',
       icon: 'mdi-file-cad-box',
       description: 'Indoor / Outdoor (Electrical drawing) lighting layout plan.      Power plug layout plan.      AC layout plan    Telephone, television, and computer drawing and layout plan.    Smoke detector and public address system drawing and layout plan.       Lightning protection layout plan.     Generator and transformer to Main Panel layout plan'
     },
     {
-      title: 'Taking pictures',
-      icon: 'mdi-desktop-mac',
-      description: 'Delivering high-end and superb pictures of myself in all different locations.'
+      title: 'Interior Design and Build Co-ordinator',
+      icon: 'mdi-account-hard-hat',
+      description: 'Managing the construction team. Managing the design process using  commercial, contractual, programme/time awareness and technical expertise. Supporting, understanding and advising the client as to the requirements and his/her obligations in the development of a building design. Encouraging and inspiring design consultants to produce their best work using their full flair, experience, resources and talents in a cost effective way. Ensuring design information is of the right quality and keeps pace with project timescales.Striving to eliminate health and safety risks in the design and financial risks during construction.'
     },
     
-    {
-      title: 'Dicka tjeter',
-      icon: 'mdi-diving',
-      description: 'nje pershkrim'
-    }
+    
   ]
 };
