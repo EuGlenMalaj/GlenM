6970b472ec7f6eec4fdaee7f471fed3a6453a400
EuGlenMalaj <euglen1991@gmail.com>
2022-07-18 18:31:21 +0200

    ndryshime ne about

--- resources/subpages/about.js
+++ resources/subpages/about.js
@@ -8,7 +8,7 @@ export default {
   description: 'Experienced Business Team Leader with a demonstrated history of working in the electrical manufacturing industry ,electrical infrastructure in civil and industrial building. Skilled in AutoCAD, Microsoft Word, Electrical Wiring, Management, and Teamwork. Strong entrepreneurship professional with a Masters degree focused in Electrical Engineering Electropower from Polytechnic University of Tirana. .',
   birthYear: '1991',
   residence: 'Albania',
-  freelance: 'Electrical Engineering',
+  freelance: 'Electrical Engineer',
   address: 'Tiranë, AL',
   quote: 'An ambitious man either wins or learns.',
   hobbies: [
