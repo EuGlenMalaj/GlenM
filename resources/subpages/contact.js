5d16b007e182adc5e4bef35392837277c2b30b44
EuGlenMalaj <euglen1991@gmail.com>
2022-07-18 18:36:10 +0200

    Ndryshim vendodhje

--- resources/subpages/contact.js
+++ resources/subpages/contact.js
@@ -4,7 +4,7 @@ export default {
   header: 'Drop a Line',
   icon: 'mdi-send',
 
-  mapsIframe: '<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d1498.306832838882!2d19.8029025!3d41.3172674!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1350318f94a72abb%3A0x8f6483c0029f0503!2sKompleksi%20Niku!5e0!3m2!1sen!2s!4v1657576000133!5m2!1sen!2s" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>',
+  mapsIframe: '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d47935.68508840105!2d19.78280378092442!3d41.3310413445573!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1350310470fac5db%3A0x40092af10653720!2sTirana%2C%20Albania!5e0!3m2!1sen!2s!4v1658162081507!5m2!1sen!2s" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>',
   address: 'Tiranë, AL',
   hours: '9AM - 5PM',
   email: 'euglen1991@gmail.com',
