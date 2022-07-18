755e1eee34d0c38ff5e37f5f96372df6e70642de
EuGlenMalaj <euglen1991@gmail.com>
2022-07-18 18:29:54 +0200

    portfolio ndryshime

--- resources/subpages/portfolio.js
+++ resources/subpages/portfolio.js
@@ -20,6 +20,10 @@ export default {
       title: '',
       icon: 'mdi-account-hard-hat',
       description: 'INTERIOR DESIGN AND BUILD CO-ORDINATOR for a 2 bedroom + kitchen + bathroom flat located in Tirana. Creating an energy-efficient project, using efficient electrical equipment and LED lighting. Installation of an efficient heating and cooling system, thermal insulation of the walls of the apartment,laying the temperature insulating floor system. and replacement of the openings of the apartment with a more efficient system. Porpouse : Designing a productive system while maintaining a cost-quality balance in such a way as to fulfill the criteria of an efficient system as well as fulfilling the clients requirements.'
+    },{
+      title: '',
+      icon: 'mdi-web',
+      description: 'Experimenting on web developing, using Netlify , Github , Visual Studio Code .'
     },
     
     
