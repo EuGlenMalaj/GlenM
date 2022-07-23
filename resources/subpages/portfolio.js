abd2130be5296e3fae1f5e6cf218e5b1150f66bd
EuGlenMalaj <euglen1991@gmail.com>
2022-07-23 21:11:57 +0200

    ndryshime ne portofol

--- resources/subpages/portfolio.js
+++ resources/subpages/portfolio.js
@@ -7,17 +7,22 @@ export default {
   footer: 'Footer , ca te duash',
   products: [
     {
-      title: 'Electrical project for a residential and service facility.\n• Location Thumanë Municipality Krujë. ',
+      title: 'Electrical project for a residential and service facility \n(2700 sq/m).\n• Location Thumanë Municipality Krujë. ',
       icon: 'mdi-file-cad-box',
-      description: '•Indoor / Outdoor (Electrical drawing) lighting layout plan. \n• Power plug layout plan. \n• Telephone, television, and computer drawing layout plan. \n• Lightning protection layout plan. \n• Technical report according to IEC, IEEE, IET RULES & STANDARDS. '
+      description: '•Indoor / Outdoor lighting layout plan. \n• Power plug layout plan. \n• Telephone, television, and computer drawing layout plan. \n• Lightning protection layout plan. \n• Technical report according to IEC, IEEE, IET RULES & STANDARDS. '
     },
     {
-      title: 'Electrical project for  AGRO TOURISM FUNCTION BUILDING 2 FLOOR + 1 BASEMENT FLOOR.\n• Location Golem Gjirokastër . ',
+      title: 'Electrical project for  AGRO TOURISM FUNCTION BUILDING 2 FLOOR + 1 BASEMENT FLOOR \n( 700 sq/m).\n• Location Golem Gjirokastër . ',
       icon: 'mdi-file-cad-box',
-      description: '• Indoor / Outdoor (Electrical drawing) lighting layout plan.\n• Power plug layout plan.\n• Telephone, television, and computer drawing layout plan.\n• Lightning protection layout plan.\n• Technical report according to IEC, IEEE, IET RULES & STANDARDS. '
+      description: '• Indoor / Outdoor  lighting layout plan.\n• Power plug layout plan.\n• Telephone, television, and computer drawing layout plan.\n• Lightning protection layout plan.\n• Technical report according to IEC, IEEE, IET RULES & STANDARDS. '
     },
     {
-      title: 'INTERIOR DESIGN AND BUILD CO-ORDINATOR for a 2 bedroom + kitchen + bathroom flat located in Tirana.',
+      title: 'Significant amount of Electrical projects for  residential and service facility \n(up to 200 sq/m). ',
+      icon: 'mdi-file-cad-box',
+      description: '• Indoor lighting layout plan.\n• Power plug layout plan.\n• Power distribution panel layout plan.\n• Earthing protection layout plan.'
+    },
+    {
+      title: 'INTERIOR DESIGN AND BUILD CO-ORDINATOR for a 2 bedroom + kitchen + bathroom flat located in Tirana \n(110 sq/m).',
       icon: 'mdi-account-hard-hat',
       description: ' \n• Creating an energy-efficient project, using efficient electrical equipment and LED lighting. \n• Installation of an efficient heating and cooling system, thermal insulation of the walls of the apartment,laying the temperature insulating floor system and replacement of the openings of the apartment with a more efficient system. \n• Purpose : Designing a productive system while maintaining a cost-quality balance in such a way as to fulfill the criteria of an efficient system as well as fulfilling the clients requirements.'
     },{
